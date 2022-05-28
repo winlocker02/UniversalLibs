d7e5ffdff3e22f82c1c36debc22767d6d44c9f20
WinLocker221 <zhenya-kopylov-02@mail.ru>
2022-05-28 11:06:55 +0800

    Update

--- src/main/java/ru/universalstudio/universallibs/utils/sql/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.universalstudio.universallibs.utils.sql;
-
-import java.sql.*;
-
-@FunctionalInterface
-public interface ResponseHandler <V, R> {
-
-    R handleResponse(V value) throws SQLException;
-}
