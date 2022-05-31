de19ae77a3fef079b9c89de5106e77f130a005c8
WinLocker221 <zhenya-kopylov-02@mail.ru>
2022-05-31 15:08:28 +0800

    Update

--- src/main/java/ru/universalstudio/universallibs/UniversalLibs.java
+++ src/main/java/ru/universalstudio/universallibs/UniversalLibs.java
@@ -1,6 +1,8 @@
 package ru.universalstudio.universallibs;
 
+import com.cryptomorin.xseries.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.winlocker.utils.*;
 
 public class UniversalLibs extends JavaPlugin {
 
